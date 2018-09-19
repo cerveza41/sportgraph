70a511c8ef3447c084393a65f850b64ca597b5b5
Hannes Marien <hannes.marien@gmail.com>
2018-09-19 20:09:29 +0200

    move functions and rename them to indicate their scope

--- sportgraph/trello.py
+++ sportgraph/trello.py
@@ -6,26 +6,12 @@ from datetime import date
 import re
 
 
-def to_int(s: str) -> int:
-    try:
-        return int(s)
-    except ValueError:
-        return 0
-
-
-def to_float(s: str) -> float:
-    try:
-        return float(s)
-    except ValueError:
-        return 0
-
-
 def extract_dates(dates: List[str]) -> List[date]:
     result = []
     for d in dates:
-        year = to_int(d[0:4])
-        month = to_int(d[5:7])
-        day = to_int(d[8:10])
+        year = __to_int(d[0:4])
+        month = __to_int(d[5:7])
+        day = __to_int(d[8:10])
         result.append(date(year, month, day))
     return sorted(result)
 
@@ -37,26 +23,12 @@ def extract_distance(comments: List[str]) -> List[float]:
         matches = pattern.match(c)
         if len(matches.regs) == 3:
             distance = '{}.{}'.format(matches.group(1), matches.group(2))
-            result.append(to_float(distance))
+            result.append(__to_float(distance))
     return result
 
 
-def get_comment_text(comment):
-    return comment['data']['text']
-
-
-def get_date_string(comment):
-    return comment['date']
-
-
-def get_api_keys():
-    with open(os.path.join(os.path.dirname(__file__), 'trello_api.json')) as f:
-        j = json.load(f)
-        return (j['key'], j['secret'], j['token'])
-
-
 def request_all_comments() ->Tuple[List[str], List[str]]:
-    key, secret, token = get_api_keys()
+    key, secret, token = __get_api_keys()
     client = TrelloClient(api_key=key, api_secret=secret, token=token)
     dates = []
     comments = []
@@ -69,6 +41,34 @@ def request_all_comments() ->Tuple[List[str], List[str]]:
                     for card in cards:
                         if card.name == 'Laufen':
                             for comment in card.get_comments():
-                                dates.append(get_date_string(comment))
-                                comments.append(get_comment_text(comment))
+                                dates.append(__get_date_string(comment))
+                                comments.append(__get_comment_text(comment))
     return (dates, comments)
+
+
+def __to_int(s: str) -> int:
+    try:
+        return int(s)
+    except ValueError:
+        return 0
+
+
+def __to_float(s: str) -> float:
+    try:
+        return float(s)
+    except ValueError:
+        return 0
+
+
+def __get_comment_text(comment):
+    return comment['data']['text']
+
+
+def __get_date_string(comment):
+    return comment['date']
+
+
+def __get_api_keys():
+    with open(os.path.join(os.path.dirname(__file__), 'trello_api.json')) as f:
+        j = json.load(f)
+        return (j['key'], j['secret'], j['token'])
